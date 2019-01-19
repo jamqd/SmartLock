340b06a4922dcddc2fe4ba35552a81b66cc48264
jamqd <john.amq.dang@gmail.com>
2019-01-19 12:20:49 -0800

    added test2.py

--- test2.py
+++ test2.py
@@ -1,31 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 19 02:13:43 2019
-
-@author: melodychen
-"""
-
 import RPi.GPIO as GPIO
 import time
-LockPin = 7
+LedPin = 7
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(LockPin, GPIO.OUT)
-    GPIO.output(LockPin, GPIO.LOW)
+    GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.LOW)
 def lock():
-    GPIO.output(LockPin, GPIO.HIGH)
-    #time.sleep(10)
+    GPIO.output(LedPin, GPIO.HIGH)
+    time.sleep(0.25)
 def unlock():
-    GPIO.output(LockPin, GPIO.LOW)
-    #time.sleep(1)
+    GPIO.output(LedPin, GPIO.LOW)
+    time.sleep(0.25)
 def destroy():
-    GPIO.output(LockPin, GPIO.LOW)
+    GPIO.output(LedPin, GPIO.LOW)
     GPIO.cleanup()
     
 if __name__ == '__main__':
     setup()
-    try:
+    for x in range(0,5):
         lock()
-    except KeyboardInterrupt:
-        destroy()
+        unlock()
+

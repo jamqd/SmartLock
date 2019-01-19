9f2e3277289548cf10801efb2cd43ab97b5bdd20
Unknown <lollypop631@gmail.com>
2019-01-19 05:37:31 -0800

    anther way to write gyro

--- gyro2.py
+++ gyro2.py
@@ -1,60 +1,169 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 19 05:14:38 2019
+import smbus			#import SMBus module of I2C
+from time import sleep          #import
 
-@author: lolly
-"""
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
 
-char filename[20];
-sprintf(filename, "/dev/i2c-%d", 1);
-file = open(filename, O_RDWR);
-if (file<0) {
-        printf("Unable to open I2C bus!");
-        exit(1);
-}
 
-// Enable accelerometer.
-writeAccReg(LSM303_CTRL_REG1_A, 0b01010111); //  z,y,x axis enabled , 100Hz data rate
-writeAccReg(LSM303_CTRL_REG4_A, 0b00101000); // +/- 8G full scale: FS = 10 on DLHC, high resolution output mode
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
-void readACC(int  *a)
-{
-    uint8_t block[6];
-    selectDevice(file,LSM9DS0_ACC_ADDRESS);
-    readBlock(0x80 |  LSM9DS0_OUT_X_L_A, sizeof(block), block);
-     
-    // Combine readings for each axis.
-    *a = (int16_t)(block[0] | block[1] << 8);
-    *(a+1) = (int16_t)(block[2] | block[3] << 8);
-    *(a+2) = (int16_t)(block[4] | block[5] << 8);
+def read_raw_data(addr):
+	#Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+    
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+        
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+MPU_Init()
+
+print (" Reading Data of Gyroscope and Accelerometer")
+
+while True:
+	
+	#Read Accelerometer raw value
+	acc_x = read_raw_data(ACCEL_XOUT_H)
+	acc_y = read_raw_data(ACCEL_YOUT_H)
+	acc_z = read_raw_data(ACCEL_ZOUT_H)
+	
+	#Read Gyroscope raw value
+	gyro_x = read_raw_data(GYRO_XOUT_H)
+	gyro_y = read_raw_data(GYRO_YOUT_H)
+	gyro_z = read_raw_data(GYRO_ZOUT_H)
+	
+	#Full scale range +/- 250 degree/C as per sensitivity scale factor
+	Ax = acc_x/16384.0
+	Ay = acc_y/16384.0
+	Az = acc_z/16384.0
+	
+	Gx = gyro_x/131.0
+	Gy = gyro_y/131.0
+	Gz = gyro_z/131.0
+	
+
+	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+	sleep(1)
  
-    
-    
+
+ 
+
+C Program
+Here, we are using WiringPi C library to read data from MPU6050 module.
+
+/*
+	MPU6050 Interfacing with Raspberry Pi
+	http://www.electronicwings.com
+*/
+
+#include <wiringPiI2C.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define Device_Address 0x68	/*Device Address/Identifier for MPU6050*/
+
+#define PWR_MGMT_1   0x6B
+#define SMPLRT_DIV   0x19
+#define CONFIG       0x1A
+#define GYRO_CONFIG  0x1B
+#define INT_ENABLE   0x38
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_ZOUT_H 0x3F
+#define GYRO_XOUT_H  0x43
+#define GYRO_YOUT_H  0x45
+#define GYRO_ZOUT_H  0x47
+
+int fd;
+
+void MPU6050_Init(){
+	
+	wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
+	wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
+	wiringPiI2CWriteReg8 (fd, CONFIG, 0);		/* Write to Configuration register */
+	wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);	/* Write to Gyro Configuration register */
+	wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+
+	} 
+short read_raw_data(int addr){
+	short high_byte,low_byte,value;
+	high_byte = wiringPiI2CReadReg8(fd, addr);
+	low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	value = (high_byte << 8) | low_byte;
+	return value;
 }
 
-//Convert Gyro raw to degrees per second
-rate_gyr_x = (float) *gyr_raw * G_GAIN;
-rate_gyr_y = (float) *(gyr_raw+1) * G_GAIN;
-rate_gyr_z = (float) *(gyr_raw+2) * G_GAIN;
-
-//Calculate the angles from the gyro
-gyroXangle+=rate_gyr_x*DT;
-gyroYangle+=rate_gyr_y*DT;
-gyroZangle+=rate_gyr_z*DT;
-
-AccXangle = (float) (atan2(*(acc_raw+1),*(acc_raw+2))+M_PI)*RAD_TO_DEG;
-AccYangle = (float) (atan2(*(acc_raw+2),*acc_raw)+M_PI)*RAD_TO_DEG;
-
-//Change the rotation value of the accelerometer to -/+ 180
-if (AccXangle > 180)
-{
-          AccXangle -= (float)360.0;
+void ms_delay(int val){
+	int i,j;
+	for(i=0;i<=val;i++)
+		for(j=0;j<1200;j++);
 }
-         if (AccYangle > 180)
-         AccYangle -= (float)360.0;
-         
- startInt = mymillis();        
-while(mymillis() - startInt < 20)
-{
-        usleep(100);
+
+int main(){
+	
+	float Acc_x,Acc_y,Acc_z;
+	float Gyro_x,Gyro_y,Gyro_z;
+	float Ax=0, Ay=0, Az=0;
+	float Gx=0, Gy=0, Gz=0;
+	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
+	MPU6050_Init();		                 /* Initializes MPU6050 */
+	
+	while(1)
+	{
+		/*Read raw value of Accelerometer and gyroscope from MPU6050*/
+		Acc_x = read_raw_data(ACCEL_XOUT_H);
+		Acc_y = read_raw_data(ACCEL_YOUT_H);
+		Acc_z = read_raw_data(ACCEL_ZOUT_H);
+		
+		Gyro_x = read_raw_data(GYRO_XOUT_H);
+		Gyro_y = read_raw_data(GYRO_YOUT_H);
+		Gyro_z = read_raw_data(GYRO_ZOUT_H);
+		
+		/* Divide raw value by sensitivity scale factor */
+		Ax = Acc_x/16384.0;
+		Ay = Acc_y/16384.0;
+		Az = Acc_z/16384.0;
+		
+		Gx = Gyro_x/131;
+		Gy = Gyro_y/131;
+		Gz = Gyro_z/131;
+		
+		printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n",Gx,Gy,Gz,Ax,Ay,Az);
+		delay(500);
+		
+	}
+	return 0;
 }

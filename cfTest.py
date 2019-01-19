93a43fe428e39e8421dd030312103a5d31faad8e
Isabel Dominik <icd107@live.com>
2019-01-19 01:50:53 -0800

    CF testing updated with person groups

--- cfTest.py
+++ cfTest.py
@@ -13,6 +13,34 @@ CF.Key.set(KEY)
 BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0'  # Replace with your regional Base URL
 CF.BaseUrl.set(BASE_URL)
 
+"""
 img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
 result = CF.face.detect(img_url)
 print (result)
+"""
+
+face_id = result[0].get('faceId')
+print (face_id)
+
+CF.person_group.delete('hi')
+
+test = CF.person_group.create('hi')
+print("smt here " + str(test))
+
+print(CF.person_group.get('hi'))
+
+person_id = CF.person.create('hi', 'bob')
+
+print(person_id)
+
+#print(CF.person.get('hi', person_id))
+
+CF.person.add_face("Users\icd10\Documents\SmartLock\detection1.jpg", 'hi', person_id)
+
+CF.person_group.delete('hi')
+"""
+
+result2 = CF.face.identify([result[0].get('faceId')], person_group_id)
+
+print(result2)
+"""

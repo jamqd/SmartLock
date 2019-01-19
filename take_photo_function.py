486679348b44487921f4ca369c6d1796996c0cde
Unknown <melodyc18113765@tas.tw>
2019-01-18 23:40:26 -0800

    renamed date time of photo taken

--- take_photo_function.py
+++ take_photo_function.py
@@ -6,17 +6,23 @@ This is a temporary script file.
 """
 #Packages to install on Picamera
 import time
+import datetime
 import picamera 
 import numpy as np
+
+    
 def takePhoto():
-    print(np.__version__)
+    currentDT = datetime.datetime.now()
+    name = str(currentDT)
+
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
         camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
-        camera.capture('face.jpg')
+        camera.capture(name + '.jpg')
 
 
 #runs the code
 takePhoto();
+        

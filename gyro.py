153f04140e3be6c3ce0cf318dd37c951d2c342c0
Unknown <lollypop631@gmail.com>
2019-01-19 05:01:35 -0800

    indented file

--- gyro.py
+++ gyro.py
@@ -48,34 +48,34 @@ bus.write_byte_data(0X6b, power_mgmt_1, 0)
 
 while True:
  
-print "Gyroskop"
-print "--------"
- 
-gyroskop_xout = read_word_2c(0x43)
-gyroskop_yout = read_word_2c(0x45)
-gyroskop_zout = read_word_2c(0x47)
- 
-print "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131)
-print "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131)
-print "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131)
- 
-print
-print "Beschleunigungssensor"
-print "---------------------"
- 
-beschleunigung_xout = read_word_2c(0x3b)
-beschleunigung_yout = read_word_2c(0x3d)
-beschleunigung_zout = read_word_2c(0x3f)
- 
-beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
-beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
-beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
- 
-print "beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert
-print "beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert
-print "beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert
- 
-print "X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
-print "Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
-
-sleep(1)
+    print "Gyroskop"
+    print "--------"
+     
+    gyroskop_xout = read_word_2c(0x43)
+    gyroskop_yout = read_word_2c(0x45)
+    gyroskop_zout = read_word_2c(0x47)
+     
+    print "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131)
+    print "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131)
+    print "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131)
+     
+    print
+    print "Beschleunigungssensor"
+    print "---------------------"
+     
+    beschleunigung_xout = read_word_2c(0x3b)
+    beschleunigung_yout = read_word_2c(0x3d)
+    beschleunigung_zout = read_word_2c(0x3f)
+     
+    beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
+    beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
+    beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+     
+    print "beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert
+    print "beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert
+    print "beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert
+     
+    print "X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+    print "Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert)
+    
+    sleep(1)
